53fc1b09355f1199cd01c17fbf5e088788d27b85
rkritzar39 <rkritzar39@gmail.com>
2026-06-25 16:10:37 -0400

    Add menu toggle functionality to top navigation

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 function showMessage(message) {
   alert(message)
 }
+
+const menuToggle = document.getElementById('menuToggle')
+const topNav = document.getElementById('topNav')
+
+if (menuToggle && topNav) {
+  menuToggle.addEventListener('click', function () {
+    topNav.classList.toggle('show')
+  })
+}
